5420998fca425281660588b805ce945e594a99f0
Leonardo Rodrigues <contato@leonardorodrigues.me>
2024-06-25 20:31:33 -0300

    feat: implements typescript

---
+++ src/components/Carousel.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="carousel">
+    carousel
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
